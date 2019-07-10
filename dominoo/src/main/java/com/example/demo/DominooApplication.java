c8fb492f5df95474298cf07c0bbe8cd2fa9143fa
moonYongEun <hafg7751@gmail.com>
2019-07-10 11:33:55 +0900

    1.dominoo project 업로드

---
+++ dominoo/src/main/java/com/example/demo/DominooApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DominooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DominooApplication.class, args);
+	}
+
+}
